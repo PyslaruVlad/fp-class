60b3402ddbd8efd880269ed679009055c6b9e776
PyslaruVlad <vlpyslaru@gmail.com>
2014-11-17 16:16:10 +0300

    10 задание. Дополнил 4 пункт.

--- class-10/05-intersect.hs
+++ class-10/05-intersect.hs
@@ -5,7 +5,6 @@ intersect = foldr1 (\ ls acc -> concat $ placeInBra `liftM` acc `ap` ls)
     where
         placeInBra e1 e2 = if e1 == e2 then [e1] else []
 
-
 chek_intersect = all (== ans) $ intersect `liftM` [case1, case2, case3]
     where
         case1 = [[1..10], [1..5], ans]
