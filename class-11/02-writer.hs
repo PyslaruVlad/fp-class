ed2fe00e5080c3dae6f6db9ecbfe666f44e17609
PyslaruVlad <vlpyslaru@gmail.com>
2014-11-22 23:33:44 +0300

    Дополнены задачи 1-3.

--- class-11/02-writer.hs
+++ class-11/02-writer.hs
@@ -1,42 +1,71 @@
 import Control.Monad.Writer
 import Control.Monad
 import Control.Applicative
+import Data.List
 
-data CalcState = CState { getSum::Double,  getMult::Double,  index::Int}
-type SeriesFunction = CalcState -> CalcState
+-- Возможно все слишком усложнил опять.
+
+type Summand = Double
+type Accum = Double
+data CalcState = CState Accum Summand Int
+type CalcFunction = CalcState -> CalcState
 type SeriesCalulation = Writer [Double] CalcState
 
-performCalc :: SeriesFunction -> SeriesCalulation -> SeriesCalulation
-performCalc func w = w >>= \ stp@(CState s m i) -> tell [m] >> return (func stp)
+performCalc :: CalcFunction -> SeriesCalulation -> SeriesCalulation
+performCalc func w = w >>= \ stp@(CState _ m _) -> tell [m] >> return (func stp)
 
-calcTaylorSeries :: Double -> Double -> SeriesFunction -> (Double, [Double])
+calcTaylorSeries :: Double -> Double -> CalcFunction -> (Double, [Double])
 calcTaylorSeries s eps func = makeAns $ runWriter $ performWhile 0 $ writer (CState 0 s 1, [])
     where
-        makeAns ((CState s m i), jrnl) = (s, jrnl)
-        performWhile prev w = performCalc func w >>= \ stp@(CState next m i) -> 
+        makeAns ((CState s _ _), jrnl) = (s, jrnl)
+        performWhile prev w = performCalc func w >>= \ stp@(CState next _ _) -> 
             if abs(prev-next) < eps then return stp else performWhile next (return stp)
 
+
+-- Ответы.
+
+calcSimpleTaylor :: Double -> Double -> (Summand -> Int -> Summand) -> (Double, [Double])
+calcSimpleTaylor eps s nextMult = calcTaylorSeries s eps (\ (CState s m i) 
+    -> CState (s+m) (nextMult m i) (i+1))
+
 calcSin :: Double -> Double -> (Double, [Double])
-calcSin eps x = calcTaylorSeries x eps (\ (CState s m i) 
-    -> CState (nextSum s m) (nextMult m i) (i+1))
-    where
-        nextSum a p = a + p
-        nextMult a n = - x * x * a / (2 * fromIntegral n) / (1 + 2 * fromIntegral n)
+calcSin eps x = calcSimpleTaylor eps x (\ a n 
+    -> - x * x * a / (2 * fromIntegral n) / (1 + 2 * fromIntegral n))
         
 calcCos :: Double -> Double -> (Double, [Double])
-calcCos eps x = calcTaylorSeries 1 eps (\ (CState s m i)
-    -> CState (nextSum s m) (nextMult m i) (i+1))
-    where
-        nextSum a p = a + p
-        nextMult a n = - x * x * a / (2 * fromIntegral n - 1) / (2 * fromIntegral n)
+calcCos eps x = calcSimpleTaylor eps 1 (\ a n 
+    -> - x * x * a / (2 * fromIntegral n - 1) / (2 * fromIntegral n))
 
-checkSinCos = compare chSins ansSins && compare chCoss ansCoss
+
+-- Проверки.
+
+args :: [Double]
+args = (\ n -> 2 * pi / 10000 * fromIntegral n) `liftM` [1..10000]
+
+eps :: Double
+eps = 0.000000000001
+
+compareZip :: ZipList Double -> ZipList Double -> Bool
+compareZip l1 l2 = and $ getZipList $ (\ a b -> abs (a-b) < eps) <$> l1 <*> l2
+
+-- Проверка совпадений значений (с заданной точностью) c заведомо правильными функциями
+-- синуса и косинуса.
+checkSinCos_onlyAnswers :: Bool
+checkSinCos_onlyAnswers = compareZip chSins ansSins && compareZip chCoss ansCoss
     where
-        args = (\ n -> 2 * pi / 10000 * fromIntegral n) <$> ZipList [1..10000]
-        eps = 0.000000000001
-        chSins = (fst . calcSin eps) <$> args
-        chCoss = (fst . calcCos eps) <$> args
-        ansSins = sin <$> args
-        ansCoss = cos <$> args
-        compare l1 l2 = and $ getZipList $ (\ a b -> abs (a-b) < eps) <$> l1 <*> l2
-        
+        makeList func = func <$> ZipList args
+        chSins = makeList (fst . calcSin eps)
+        chCoss = makeList (fst . calcCos eps)
+        ansSins = makeList sin
+        ansCoss = makeList cos
+
+-- Проверка равенста сумм от списков справа и значений слева.
+checkSinCos_sums :: Bool
+checkSinCos_sums = compareVals calcCos && compareVals calcSin
+    where
+        compareVals func = compareUnziped $ unzip $ (findSum . func eps) `liftM` args
+        findSum (val, ls) = (val, sum ls)
+        compareUnziped (ls1, ls2) = compareZip (ZipList ls1) (ZipList ls2)
+
+
+
--- class-11/03-state.hs
+++ class-11/03-state.hs
@@ -16,7 +16,15 @@ apply Deq = dequeue >> return () -- На результат смотрим по 
 applyToList :: [Operation] -> [Int] -> [Int]
 applyToList ops ls = execState (mapM apply ops) ls
 
-checkQueue = [4,1,2] == finList
+-- Проверочка.
+
+checkQueue :: Bool
+checkQueue = map finList [1,2,3] == map ans [1,2,3]
     where
-        finList = applyToList [Enq 1, Enq 2, Deq] [3,4]
+        finList 1 = applyToList [Enq 1, Enq 2, Deq] [3,4]
+        finList 2 = applyToList [Enq 7, Enq 6, Deq, Enq 5, Deq] [7,8]
+        finList 3 = applyToList [Enq 5, Deq, Enq 8, Enq 9, Deq] [10]
+        ans 1 = [4,1,2]
+        ans 2 = [7,6,5]
+        ans 3 = [8,9]
 
