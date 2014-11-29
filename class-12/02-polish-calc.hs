180264d1ec6c9f53dca4745ad6571a331c45b3d4
PyslaruVlad <vlpyslaru@gmail.com>
2014-11-29 23:55:06 +0300

    Вторая задача.

--- class-12/02-polish-calc.hs
+++ class-12/02-polish-calc.hs
@@ -25,6 +25,13 @@
 
 import Control.Monad
 import Control.Monad.State
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
+import Control.Monad.Writer
+import Control.Monad.Reader
+
+import Data.Maybe
 
 type Stack = [Int]
 
@@ -41,3 +48,58 @@ evalRPN xs = head $ execState (mapM step $ words xs) []
     step "*" = processTops (*)
     step  n  = push (read n)
     processTops op = op `liftM` pop `ap` pop >>= push
+    
+
+
+    
+-- Решение.
+
+incorrectCondition :: Stack -> String -> Bool
+incorrectCondition stack c = isOperation c && length stack < 2
+    where 
+        isOperation e = e `elem` operations
+        operations = ["+","*"]
+
+performStep :: String -> ReaderT (String -> Maybe Int) (WriterT (Sum Int) (State Stack)) ()
+performStep "+" = lift $ processTops (+)
+performStep "*" = lift $ processTops (*)
+performStep n  = do
+    lookupFunc <- ask
+    let 
+        lookRes = lookupFunc n
+        newVal = if isNothing lookRes then read n else fromJust lookRes
+    lift $ lift $ push newVal
+
+processTops :: (Int -> Int -> Int) -> WriterT (Sum Int) (State Stack) ()
+processTops op = do
+    tell (Sum 1) -- количество успешных операций со стеком.
+    lift $ op `liftM` pop `ap` pop >>= push
+
+processSymbol :: String -> MaybeT (ReaderT (String -> Maybe Int) 
+    (WriterT (Sum Int) (State Stack))) ()
+processSymbol c = do
+    stack <- get
+    if incorrectCondition stack c
+    then mzero
+    else lift $ performStep c
+
+newEvalRPN :: (String -> Maybe Int) -> [String] -> (Maybe Int, Int)
+newEvalRPN lookupFunc = checkMaybe . runAll . runMaybeT . mapM processSymbol
+    where 
+        checkMaybe ((Just _, c), stack) = (Just $ head stack, getSum c)
+        checkMaybe ((Nothing, c), _) = (Nothing, getSum c)
+        runAll = (flip runState []) . runWriterT . (flip runReaderT lookupFunc)
+
+
+
+
+
+-- Тесты.
+
+
+testRPN = positiveTest && negativeTest
+    where
+        positiveTest = newEvalRPN dictFunc (words "3 2 4 + *") == (Just 32, 2)
+        negativeTest = newEvalRPN dictFunc (words "3 2 4 + * *") == (Nothing, 2)
+        dictFunc = \_ -> Just 4
+
