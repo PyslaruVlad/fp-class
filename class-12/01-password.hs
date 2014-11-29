bb770c052e4c07323c916bca4c7e11e7a8a5010b
PyslaruVlad <vlpyslaru@gmail.com>
2014-11-29 22:04:53 +0300

    Первая задача.

--- class-12/01-password.hs
+++ class-12/01-password.hs
@@ -1,3 +1,4 @@
+{-# Language TypeSynonymInstances, FlexibleInstances, OverlappingInstances #-}
 {-
    Модифицируйте представленное на лекции решение задачи о запросе пароля,
    удовлетворяющего требованиям по стойкости, следующим образом:
@@ -11,8 +12,12 @@
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
+import Control.Monad.Writer
+import Control.Monad.Reader
 
 import Data.Char
+import Data.Maybe
+import System.Environment
 
 isValid :: String -> Bool
 isValid s = length s >= 8 && 
@@ -32,4 +37,55 @@ askPassword = do
   value <- msum $ repeat getValidPassword
   lift $ putStrLn "Сохранение в базе данных..."
 
-main = runMaybeT askPassword
+-- main = runMaybeT askPassword
+
+
+-- То, что дописал я.
+
+type Predicate a = a -> Bool
+
+checkingPassword :: MaybeT (ReaderT (Predicate String) (WriterT [String] IO)) String
+checkingPassword = do
+    liftIO $ putStr "Введите новый пароль: "
+    w <- liftIO $ getLine
+    tell [w]
+    pred <- ask
+    if pred w
+    then (do
+        liftIO $ putStrLn "Подходящий пароль!"
+        return w)
+    else (do
+        liftIO $ putStrLn "Пароль не подходит :("
+        mzero)
+
+main = do
+    args <- getArgs
+    let 
+        pred = mconcat $ parseArgs args
+    (val, log) <- runWriterT $ flip runReaderT pred $ runMaybeT $ msum $ repeat checkingPassword
+    putStrLn "Сохранение в базе данных..."
+    putStr "Сохранение завершено. Текущий пароль: "
+    print $ fromJust val
+    putStr "Установлен после попыток: "
+    print $ reverse log
+
+
+
+-- Получение предиката по строке аргументов.
+
+-- Объединение нескольких предикатов.
+instance Monoid (Predicate a) where
+    mappend p1 p2 = \ s -> p1 s && p2 s
+    mempty = \ _ -> True
+
+-- Чтение слов из строки аргументов и получение предикатов.
+parseArgs :: [String] -> [Predicate String]
+parseArgs [] = []
+parseArgs ("-C":end) = (any isAlpha) : parseArgs end 
+parseArgs ("-N":end) = (any isNumber) : parseArgs end 
+parseArgs ("-P":end) = (any isPunctuation) : parseArgs end
+parseArgs ("-M":valStr:end) = (\ s -> read valStr <= length s) : parseArgs end
+parseArgs _ = error "Строка аргументов некорректна!"
+
+
+        
