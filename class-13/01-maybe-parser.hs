0c764115228a6a77fb98f8eb587289bbddc8d504
PyslaruVlad <vlpyslaru@gmail.com>
2014-12-07 17:37:25 +0300

    complete 13.1

--- class-13/01-maybe-parser.hs
+++ class-13/01-maybe-parser.hs
@@ -1,3 +1,6 @@
+import Control.Monad
+import Control.Applicative
+
 {-
    Тип Parser может быть определён следуюшим образом:
 -}
@@ -9,10 +12,41 @@ newtype Parser a = Parser { apply :: String -> Maybe (a, String) }
 -}
 
 instance Monad Parser where
-  return x = undefined
-  p >>= q = undefined
-  fail _ = undefined
+  return x = Parser (\ s -> Just (x,s))
+  p >>= q = Parser $ \ s0 -> case (apply p s0) of
+    Nothing -> Nothing
+    Just (r1, s1) -> apply (q r1) s1
+  fail _ = Parser (\ _ -> Nothing)
 
 instance MonadPlus Parser where
-  mzero = undefined
-  p `mplus` q = undefined
+  mzero = Parser (\ _ -> Nothing)
+  p `mplus` q = Parser (\ s -> msum $ apply <$> [p,q] <*> [s])
+
+
+
+-- Простенькие тесты.
+
+getc :: Parser Char
+getc = Parser f
+  where
+    f [] = Nothing
+    f (c:cs) = Just (c, cs)
+
+testParsMonad :: Bool
+testParsMonad = (jR == jT) && nothingCheck && (cT == cR)
+  where
+    jT = apply getc <$> ["abc", "x123", "123"]
+    jR = Just <$> [('a',"bc"),('x',"123"),('1',"23")]
+    nothingCheck = (apply getc "") == Nothing
+    cT = flip apply "12end" $ getc >>= \ _ -> getc
+    cR = Just ('2',"end")
+
+testParsPlus :: Bool
+testParsPlus = (jT == answer) && (nT == answer)
+  where
+    fPos = msum $ replicate 3 getc
+    fNeg = mzero `mplus` getc `mplus` getc `mplus` mzero
+    jT = apply fPos <$> testLists
+    nT = apply fNeg <$> testLists
+    testLists = ["abc", "x123", "12345"]
+    answer = Just <$> [('a',"bc"),('x',"123"),('1',"2345")]
--- class-13/02-float_complex.hs
+++ class-13/02-float_complex.hs
@@ -1,26 +1,37 @@
 import Parser
 import SimpleParsers
 import ParseNumbers
+import Control.Monad
 
 {- Напишите парсер для вещественных чисел. -}
+
 float :: Parser Float
-float = undefined
+float = do
+  let
+    posSum = foldl (\ acc n -> 10*acc + fromIntegral n) 0
+    negSum = foldr (\ n acc -> 0.1*(acc + fromIntegral n)) 0
+  sign <- optional True $ symbol "-" >> return False
+  pos <- posSum `liftM` many digit
+  char '.'
+  neg <- negSum `liftM` many digit
+  return $ if sign then pos + neg else -(pos + neg)
 
 {-
   Напишите парсер для представления комплексных чисел,
   записываемых в виде вещественной и мнимой части через запятую
   в круглых скобках, например, "(2.3, 1)".
-  
+
 -}
 complex :: Parser (Float, Float)
-complex = undefined
+complex = bracket "(" ")" $ sepByComma (token float) (symbol ",")
+  where sepByComma p sep = p >>= \ a -> sep >> p >>= \ b -> return (a, b)
 
 {-
   Напишите парсер для списка комплексных чисел (разделитель — точка с запятой),
   заключённого в квадратные скобки.
 -}
 complexList :: Parser [(Float, Float)]
-complexList = undefined
+complexList = bracket "[" "]" $ sepBy (token complex) (symbol ";")
 
 {-
   Модифицируйте предыдущий парсер таким образом, чтобы в исходной строке
@@ -28,7 +39,8 @@ complexList = undefined
   при этом должна считаться равной нулю).
 -}
 complexList2 :: Parser [(Float, Float)]
-complexList2 = undefined
+complexList2 = bracket "[" "]" $ sepBy tok (symbol ";")
+  where tok = token complex `mplus` (token float >>= \ a -> return (a,0))
 
 {-
    Модифицируйте предыдущий парсер таким образом, чтобы компоненты списка
@@ -36,6 +48,49 @@ complexList2 = undefined
    требуемое с помощью вспомогательных парсеров, допускающих повторное применение.
 -}
 complexList3 :: Parser [(Float, Float)]
-complexList3 = undefined
+complexList3 = bracket "[" "]" $ sepBy tok (symbol ",")
+  where tok = token complex `mplus` (token float >>= \ a -> return (a,0))
 
+-- Тесты.
 
+testFloat :: Bool
+testFloat =  posCheck && negCheck
+  where
+    posCheck = (parse float) `map` posTest == posAns
+    posTest = ["4.6", "-0.35", "2.41", "5.", "-.2"]
+    posAns = [4.6, -0.35, 2.41, 5.0, -0.2]
+    negTest = ["4.-6", "3", "-asdas", "bcd", "5.6b", "5.b"]
+    negAns = [(4.0,"-6"), (5.6,"b"), (5.0,"b")]
+    negCheck = (apply float) `concatMap` negTest == negAns
+
+testComplex :: Bool
+testComplex = posCheck && negCheck
+  where
+    posCheck = (parse complex) `map` posTest == posAns
+    posTest = ["(4.6, -0.35)", " ( 2.41, 5.)", "(-.2 , -4.19)"]
+    posAns = [(4.6,-0.35), (2.41, 5.0), (-0.2,-4.19)]
+    negTest = ["(4.-6,-asdas)", "bcd", "(5.6b)", "(5.b, 4.19)"]
+    negCheck = (apply float) `concatMap` negTest == []
+
+testLists :: Bool
+testLists = posCheck && negCheck
+  where
+    posCheck = parse complexList posTest == posAns
+    posTest = "[(4.6, -0.35); ( 2.41, 5.) ;(-.2 , -4.19)]"
+    posAns = [(4.6,-0.35), (2.41, 5.0), (-0.2,-4.19)]
+    negTest = ["[(1,-1),(2,4)]", "[();()]", "[(5.6);(3,4)]", "[4.5]"]
+    negCheck = (apply float) `concatMap` negTest == []
+
+testLists2 :: Bool
+testLists2 = posCheck
+  where
+    posCheck = parse complexList2 posTest == posAns
+    posTest = "[4.5 ;(-.2 , -4.19); 6.7 ; (1.,1.)]"
+    posAns = [(4.5,0),(-0.2,-4.19),(6.7,0), (1,1)]
+
+testLists3 :: Bool
+testLists3 = posCheck
+  where
+    posCheck = parse complexList3 posTest == posAns
+    posTest = "[4.5 ,(-.2 , -4.19), 6.7 , (1.,1.)]"
+    posAns = [(4.5,0),(-0.2,-4.19),(6.7,0), (1,1)]
