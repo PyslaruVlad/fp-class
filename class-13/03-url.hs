51c7c7a671c553535b7529cb35f629046a6ee899
PyslaruVlad <vlpyslaru@gmail.com>
2014-12-08 03:20:38 +0300

    complete 13.3

--- class-13/03-url.hs
+++ class-13/03-url.hs
@@ -1,7 +1,10 @@
+{-# Language RankNTypes #-}
 import Parser
 import SimpleParsers
 import Control.Applicative hiding (many, optional)
 import Control.Monad
+import ParseNumbers
+import Control.Monad.Writer
 
 {-
    Определите тип данных, представляющий адрес URL следующего вида:
@@ -15,7 +18,7 @@ import Control.Monad
 
 
 data Scheme = FTP | HTTP | HTTPS | Unk String
-              deriving Show
+              deriving (Show, Eq) -- Eq для тестирования.
 type Server = String
 type Path = String
 data URL = URL Scheme Server Path
@@ -30,3 +33,88 @@ url = URL <$>
       scheme <*>
       (string "://" >> many1 (sat (/='/'))) <*>
       many (sat $ const True)
+
+
+
+-- Решение.
+
+data Item = UrlScheme Scheme
+  | Login String | Password String
+  | Host String | Port Int
+  | FPath String
+  | Query String
+  | Anchor String
+  deriving (Show, Eq)
+
+data ParserTree =
+  PTNode {
+    start::String,
+    parser::Parser Item,
+    nodes::[ParserTree]
+  } |
+  PTLeaf
+
+evalTree :: ParserTree -> WriterT [Item] Parser ()
+evalTree PTLeaf = return ()
+evalTree node = do
+  lift $ string $ start node
+  res <- lift $ parser $ node
+  tell [res]
+  msum $ map evalTree $ nodes node
+
+takeUntil :: [Char] -> Parser String
+takeUntil endS = many $ sat $ \ a -> and $ map (/=a) endS
+
+liftP :: (String -> Item) -> [Char] -> Parser Item
+liftP f e = f <$> takeUntil e
+
+readURL :: String -> [Item]
+readURL = parse $ execWriterT $ evalTree mainTree
+
+
+-- Не смог придумать как обобщить дерево.
+
+-- Тип всех функций - Parser Item.
+portParser = Port <$> natural
+pswdParser = Password <$> takeUntil "@"
+hostParser = Host <$> takeUntil ":/"
+lognParser = Login <$> do
+  a <- takeUntil ":@"
+  msum $ map char ":@"
+  return a
+
+-- Тип для функций - ParserTree.
+mainTree = PTNode "" (UrlScheme <$> scheme) [lognTree, hostTree "://"]
+lognTree = PTNode "://" lognParser [pswdTree "", hostTree ""]
+pswdTree i = PTNode i pswdParser [hostTree "@"]
+portTree = PTNode ":" portParser [tailTree, PTLeaf]
+hostTree i = PTNode i hostParser [portTree, tailTree,PTLeaf]
+
+-- Тип для функций - Parser Item.
+pathParser = FPath <$> takeUntil "#?"
+queryParser = Query <$> takeUntil "#"
+anchorParser = Anchor <$> takeUntil "?"
+
+-- Тип для функций - ParserTree.
+tailTree = PTNode "/" pathParser [queryTree, anchorTree, PTLeaf]
+queryTree = PTNode "?" queryParser [anchorTree, PTLeaf]
+anchorTree = PTNode "#" anchorParser [PTLeaf]
+
+
+
+
+-- Тесты.
+
+test = readURL `map` testingSetPos == testingAnswers
+
+testingSetPos =
+  [ "xxx://site/path?query"
+  , "http://usr:pas@site"
+  , "x://usr@site:65535"
+  , "ftp://site/path#anch"]
+
+testingAnswers =
+  [[UrlScheme (Unk "xxx"), Host "site", FPath "path", Query "query"]
+  ,[UrlScheme HTTP,Login "usr",Password "pas",Host "site"]
+  ,[UrlScheme (Unk "x"),Login "usr",Host "site",Port 65535]
+  ,[UrlScheme FTP,Host "site",FPath "path",Anchor "anch"]]
