da4906eab07ed23a1dd11f8e8200350838300874
PyslaruVlad <vlpyslaru@gmail.com>
2014-12-08 04:21:41 +0300

    задача 13.5

--- class-13/05-expr-compl.hs
+++ class-13/05-expr-compl.hs
@@ -11,7 +11,7 @@ import Control.Monad
    проанализировать).
 -}
 
-data Expr = Con Int | Bin Op Expr Expr
+data Expr = Con Float | Bin Op Expr Expr
   deriving Show
 data Op = Plus | Minus | Mul | Div
   deriving Show
@@ -24,9 +24,10 @@ addop  ::= '+' | '-'
 mulop  ::= '*' | '/'
 -}
 
+expr :: Parser Expr
 expr = token (term >>= rest addop term)
   where
-    rest op unit e1 = optional e1 $ do 
+    rest op unit e1 = optional e1 $ do
         p <- op
         e2 <- unit
         rest op unit $ Bin p e1 e2
@@ -37,6 +38,49 @@ expr = token (term >>= rest addop term)
     binop (s1, cons1) (s2, cons2) =
           (symbol s1 >> return cons1) <|>
           (symbol s2 >> return cons2)
-    constant = Con `liftM` natural
+    constant = Con `liftM` float
 
 
+-- Решение.
+
+-- Из второго задания.
+float :: Parser Float
+float = do
+  let
+    posSum = foldl (\ acc n -> 10*acc + fromIntegral n) 0
+    negSum = foldr (\ n acc -> 0.1*(acc + fromIntegral n)) 0
+  sign <- optional True $ symbol "-" >> return False
+  pos <- posSum `liftM` many digit
+  char '.'
+  neg <- negSum `liftM` many digit
+  return $ if sign then pos + neg else -(pos + neg)
+
+-- Данные для представления комплексных чисел.
+data CompExpr = Real Expr | Complex (Expr, Expr) | CBin Op CompExpr CompExpr
+  deriving Show
+
+-- Парсер.
+complex :: Parser CompExpr
+complex = simple <|> complex
+  where
+    complex = bracket "(" ")" $ sepByComma (token expr) (symbol ",")
+    simple = Real <$> expr
+    sepByComma p sep = do
+      a <- p
+      sep
+      b <- p
+      return $ Complex (a, b)
+
+
+
+-- Тесты.
+
+exprs :: [String]
+exprs =
+  [ "(4.5 * (-0.2) , 2.18 - 0.2)"
+  , "4.5"
+  , "(13./3., (-0.15) * 1.)"
+  ]
+
+test :: [CompExpr]
+test = parse complex `map` exprs
