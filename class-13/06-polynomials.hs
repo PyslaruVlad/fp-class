01c7d30af3a23f3dc594acde0443a6702aeb18de
PyslaruVlad <vlpyslaru@gmail.com>
2014-12-09 16:23:16 +0300

    Задание 13.6. Определена функция GCD и тесты.

--- class-13/06-polynomials.hs
+++ class-13/06-polynomials.hs
@@ -18,8 +18,6 @@ data Element = El Float Int
 
 data Sign = Minus | Plus
 
-eps = 0.000001 -- не смог без этого.
-
 {-
   Реализуйте парсер для многочленов (примеры в файле poly.txt).
 -}
@@ -75,14 +73,14 @@ divmod = iterF
   where
     liftCon e (es, p) = (e:es, p)
     iterF p1 p2
-      | p1 < p2 = ([], p1)
+      | p1 `compMaxes` p2 == LT = ([], p1)
       | otherwise  = liftCon newQ $ iterF newD p2
       where
         newD = p1 `subPoly` (newQ `multPoly` p2)
         newQ = p1 `divPolyMax` p2
 
 divPolyMax :: Poly -> Poly -> Element
-divPolyMax es1 es2 = (maximum es1) `divEl` (maximum es2)
+divPolyMax es1 es2 = (maxPow es1) `divEl` (maxPow es2)
   where (El v1 p1) `divEl` (El v2 p2) = El (v1/v2) (p1 - p2)
 
 multPoly :: Element -> Poly -> Poly
@@ -103,21 +101,32 @@ zipPolyWith zF lF rF p1 p2 = iterF pes1 pes2
     iterF es1 [] = map lF es1
     iterF [] es2 = map rF es2
     iterF s1@(e1:es1) s2@(e2:es2)
-      | e1 > e2   = lF e1 : iterF es1 s2
-      | e1 < e2   = rF e2 : iterF s1 es2
+      | e1 `compPows` e2 == GT  = lF e1 : iterF es1 s2
+      | e1 `compPows` e2 == LT  = rF e2 : iterF s1 es2
       | otherwise = zF e1 e2 : iterF es1 es2
 
+compPows :: Element -> Element -> Ordering
+compPows (El _ p1) (El _ p2)
+  | p1 < p2  = LT
+  | p1 > p2  = GT
+  | p1 == p2 = EQ
+
+maxPow :: Poly -> Element
+maxPow = foldr1 $ \ acc ce -> if compPows acc ce /= GT then ce else acc
+
+compMaxes :: Poly -> Poly -> Ordering
+compMaxes [] [] = EQ
+compMaxes _  [] = GT
+compMaxes [] _  = LT
+compMaxes es1 es2 = maxPow es1 `compPows` maxPow es2
+
+eps = 0.000001 -- не смог без этого.
+
 instance Eq Element where
-  (El _ p1) == (El _ p2) = p1 == p2
-
-instance Ord Element where
-  (El _ p1) <= (El _ p2) = p1 <= p2
-
-instance Ord Poly where
-    es1 <= es2 = (maximum es1) <= (maximum es2)
+  (El v1 p1) == (El v2 p2) = abs (v1-v2) < eps && p1 == p2
 
 sortPoly :: Poly -> Poly
-sortPoly es = sortBy comp es
+sortPoly es = sortBy comp es -- сортировка в порядке возрастания.
   where
     comp (El _ p1) (El _ p2)
       | p1 > p2 = LT
@@ -128,7 +137,12 @@ sortPoly es = sortBy comp es
    Напишите функцию, которая вычисляет наибольший общий делитель двух многочленов.
 -}
 poly_gcd :: Poly -> Poly -> Poly
-poly_gcd = undefined
+poly_gcd [] p2 = p2
+poly_gcd p1 [] = p1
+poly_gcd p1 p2
+  | pMod == p1 = p1
+  | otherwise  = poly_gcd p2 pMod
+  where pMod = snd $ p1 `divmod` p2
 
 {-
    Напишите функцию, которая вычисляет наибольший общий делитель списка многочленов.
@@ -162,8 +176,15 @@ testingChains =
   , "x-3"
   ]
 
-testPolyParsing :: [Poly]
-testPolyParsing = (sortPoly . parse poly) `map` testingChains
+testPolyParsing :: Bool
+testPolyParsing = (sortPoly . parse poly) `map` testingChains == answer
+  where
+    answer =
+      [[El 5.0 3,El (-7.0) 1,El 2.0 0]
+      ,[El 2.0 6,El 1.0 3,El (-3.0) 2,El 1.0 0]
+      ,[El 1.0 2,El 1.0 1,El (-3.0) 0]
+      ,[El 1.0 3,El (-12.0) 2,El (-42.0) 0]
+      ,[El 1.0 1,El (-3.0) 0]]
 
 testDivmod :: Bool
 testDivmod = divmod p1 p2 == answer
@@ -171,3 +192,18 @@ testDivmod = divmod p1 p2 == answer
     p1 = (sortPoly . parse poly) "x^3 - 12x^2 - 42"
     p2 = (sortPoly . parse poly) "x-3"
     answer = ([El 1 2, El (-9) 1, El (-27) 0], [El (-123) 0])
+
+testGCD :: Bool
+testGCD = poly_gcd mikhp1 mikhp2 == answer
+  where
+    -- каюсь. эти полиномы украл у Михайличенко. зато результат такой же.
+    mikhp1 = (sortPoly . parse poly) "x^6-4x^5+2x^4+5x^3+2x^2-4x-8"
+    mikhp2 = (sortPoly . parse poly) "x^5-x^4-x^3+x^2-4x-4"
+    answer = [El (-1008.0) 2, El 1008.0 1, El 2016.0 0]
+
+testGCDList :: Bool
+testGCDList = isConstant $ poly_gcd_list list -- получается константа.
+  where
+    list = (sortPoly . parse poly) `map` testingChains
+    isConstant [El _ 0] = True
+    isConstant _ = False
